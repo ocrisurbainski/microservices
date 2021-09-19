4198e8974a355dc525a40f62a7756d80c90835c8
Cristian Urbainski <cristianurbainskips@gmail.com>
2021-09-19 15:22:57 -0300

    Adicionado o eureka client ao projeto de produtos para que possa se registrar no eureka e ficar disponível para outros serviços que seja necesário.

--- produtos/src/main/java/br/com/urbainski/microservices/produtos/ProdutosApplication.java
+++ produtos/src/main/java/br/com/urbainski/microservices/produtos/ProdutosApplication.java
@@ -3,8 +3,10 @@ package br.com.urbainski.microservices.produtos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class ProdutosApplication {
 
