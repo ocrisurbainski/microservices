8aee73dc689108bf0c819c72583b62ce90ae76f9
Cristian Urbainski <cristianurbainskips@gmail.com>
2021-09-19 23:12:07 -0300

    Adicionado projeto do gateway.

---
+++ gateway/src/main/java/br/com/urbainski/microservices/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package br.com.urbainski.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Cristian Urbainski
+ * @since 19/09/2021
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
